e5588e0291ab4eeb166e74692af19db7f83ee763
will3 <will3.git@gmail.com>
2016-01-01 13:56:45 +1300

    Add hooks for commonjs and amd

--- cpr.js
+++ cpr.js
@@ -1,98 +1,109 @@
-var $ = require('jquery');
-var tinycolor = require('tinycolor2');
+(function(factory) {
+  "use strict";
 
-function invertColor(hexTripletColor) {
-  var color = hexTripletColor;
-  color = color.substring(1); // remove #
-  color = parseInt(color, 16); // convert to integer
-  color = 0xFFFFFF ^ color; // invert three bytes
-  color = color.toString(16); // convert to hex
-  color = ("000000" + color).slice(-6); // pad with leading zeros
-  color = "#" + color; // prepend #
-  return color;
-}
+  if (typeof define === 'function' && define.amd) { // AMD
+    define(['jquery'], factory);
+  } else if (typeof exports == "object" && typeof module == "object") { // CommonJS
+    module.exports = factory(require('jquery'));
+  } else { // Browser
+    factory(jQuery);
+  }
+})(function($, undefined) {
+  var tinycolor = require('tinycolor2');
 
-var makeBlock = function() {
-  return $('<div/>').addClass('cpr-block cpr-unselectable');
-};
-
-var cpr = function(options) {
-  var root = $('<div/>');
-  var lastBlock = null;
-
-  var addBlock = function(color) {
-    var block = makeBlock();
-    block.css('background-color', color);
-    if (lastBlock === null) {
-      root.append(block);
-    } else {
-      block.insertAfter(lastBlock);
-    }
-    lastBlock = block;
-  };
-
-  var palette = options.palette || [];
-
-  root.addClass('cpr-container cpr-unselectable');
-
-  document.body.appendChild(root.get()[0]);
-
-  for (var i = 0; i < palette.length; i++) {
-    var color = tinycolor(palette[i]).toHexString();
-    addBlock(color);
+  function invertColor(hexTripletColor) {
+    var color = hexTripletColor;
+    color = color.substring(1); // remove #
+    color = parseInt(color, 16); // convert to integer
+    color = 0xFFFFFF ^ color; // invert three bytes
+    color = color.toString(16); // convert to hex
+    color = ("000000" + color).slice(-6); // pad with leading zeros
+    color = "#" + color; // prepend #
+    return color;
   }
 
-  var preview = makeBlock().attr("id", "cpr-preview");
-  root.append(preview);
+  var makeBlock = function() {
+    return $('<div/>').addClass('cpr-block cpr-unselectable');
+  };
 
-  root.append($('<br>'));
+  var cpr = function(options) {
+    var root = $('<div/>');
+    var lastBlock = null;
 
-  var placeholder = 'ffffff';
+    var addBlock = function(color) {
+      var block = makeBlock();
+      block.css('background-color', color);
+      if (lastBlock === null) {
+        root.append(block);
+      } else {
+        block.insertAfter(lastBlock);
+      }
+      lastBlock = block;
+    };
 
-  var input = $('<input class="cpr-input" placeholder="f6f6f6" type="text"></input>');
+    var palette = options.palette || [];
 
-  root.append(input);
+    root.addClass('cpr-container cpr-unselectable');
 
-  $(document).on('click', '.cpr-container .cpr-block', function(event) {
-    var color = $(this).css('background-color');
-    if (options.click !== undefined) {
-      options.click(tinycolor(color));
+    document.body.appendChild(root.get()[0]);
+
+    for (var i = 0; i < palette.length; i++) {
+      var color = tinycolor(palette[i]).toHexString();
+      addBlock(color);
     }
-    event.stopPropagation();
-  });
 
-  input.focus(function() {
-    if (options.focus !== undefined) {
-      options.focus();
-    }
-  });
+    var preview = makeBlock().attr("id", "cpr-preview");
+    root.append(preview);
 
-  input.blur(function() {
-    if (options.blur !== undefined) {
-      options.blur();
-    }
-  });
+    root.append($('<br>'));
 
-  input.on('input', function() {
-    var value = input.val();
-    var color = tinycolor(value).toHexString();
-    if (value.length >= 3) {
-      preview.show();
-    } else {
-      preview.hide();
-    }
-    $('#cpr-preview').css('background-color', color);
-  });
+    var placeholder = 'ffffff';
 
-  input.keyup(function(e) {
-    if (e.keyCode === 13) {
+    var input = $('<input class="cpr-input" placeholder="f6f6f6" type="text"></input>');
+
+    root.append(input);
+
+    $(document).on('click', '.cpr-container .cpr-block', function(event) {
+      var color = $(this).css('background-color');
+      if (options.click !== undefined) {
+        options.click(tinycolor(color));
+      }
+      event.stopPropagation();
+    });
+
+    input.focus(function() {
+      if (options.focus !== undefined) {
+        options.focus();
+      }
+    });
+
+    input.blur(function() {
+      if (options.blur !== undefined) {
+        options.blur();
+      }
+    });
+
+    input.on('input', function() {
       var value = input.val();
-      addBlock(tinycolor(value).toHexString());
-      input.val('');
-      preview.hide();
-      input.blur();
-    }
-  });
+      var color = tinycolor(value).toHexString();
+      if (value.length >= 3) {
+        preview.show();
+      } else {
+        preview.hide();
+      }
+      $('#cpr-preview').css('background-color', color);
+    });
 
-  preview.hide();
-};
+    input.keyup(function(e) {
+      if (e.keyCode === 13) {
+        var value = input.val();
+        addBlock(tinycolor(value).toHexString());
+        input.val('');
+        preview.hide();
+        input.blur();
+      }
+    });
+
+    preview.hide();
+  };
+}();
